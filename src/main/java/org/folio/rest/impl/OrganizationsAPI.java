3686a5d8554769bf5c16ca4b058e5f3e8539d131
Aliaksandr Pautau <aliaksandr_pautau@epam.com>
2019-04-12 12:07:00 +0300

    MODORGS-4 Refactor APIs
    - updated APIs implementation

--- src/main/java/org/folio/rest/impl/OrganizationsAPI.java
+++ src/main/java/org/folio/rest/impl/OrganizationsAPI.java
@@ -7,7 +7,7 @@ import io.vertx.core.Vertx;
 import org.folio.rest.annotations.Validate;
 import org.folio.rest.jaxrs.model.Organization;
 import org.folio.rest.jaxrs.model.OrganizationCollection;
-import org.folio.rest.jaxrs.resource.OrganizationStorageOrganizations;
+import org.folio.rest.jaxrs.resource.OrganizationsStorageOrganizations;
 import org.folio.rest.persist.EntitiesMetadataHolder;
 import org.folio.rest.persist.PgUtil;
 import org.folio.rest.persist.PostgresClient;
@@ -18,7 +18,7 @@ import java.util.Map;
 
 import static org.folio.rest.persist.HelperUtils.getEntitiesCollection;
 
-public class OrganizationsAPI implements OrganizationStorageOrganizations {
+public class OrganizationsAPI implements OrganizationsStorageOrganizations {
   private static final String ORGANIZATION_TABLE = "organizations";
 
   private String idFieldName = "id";
@@ -28,9 +28,9 @@ public class OrganizationsAPI implements OrganizationStorageOrganizations {
   }
 
   @Override
-  public void getOrganizationStorageOrganizations(String query, int offset, int limit, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+  public void getOrganizationsStorageOrganizations(String query, int offset, int limit, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     vertxContext.runOnContext((Void v) -> {
-      EntitiesMetadataHolder<Organization, OrganizationCollection> entitiesMetadataHolder = new EntitiesMetadataHolder<>(Organization.class, OrganizationCollection.class, GetOrganizationStorageOrganizationsResponse.class);
+      EntitiesMetadataHolder<Organization, OrganizationCollection> entitiesMetadataHolder = new EntitiesMetadataHolder<>(Organization.class, OrganizationCollection.class, GetOrganizationsStorageOrganizationsResponse.class);
       QueryHolder cql = new QueryHolder(ORGANIZATION_TABLE, query, offset, limit);
       getEntitiesCollection(entitiesMetadataHolder, cql, asyncResultHandler, vertxContext, okapiHeaders);
     });
@@ -38,29 +38,29 @@ public class OrganizationsAPI implements OrganizationStorageOrganizations {
 
   @Override
   @Validate
-  public void postOrganizationStorageOrganizations(String lang, Organization entity,
+  public void postOrganizationsStorageOrganizations(String lang, Organization entity,
                                         Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    PgUtil.post(ORGANIZATION_TABLE, entity, okapiHeaders, vertxContext, PostOrganizationStorageOrganizationsResponse.class, asyncResultHandler);
+    PgUtil.post(ORGANIZATION_TABLE, entity, okapiHeaders, vertxContext, PostOrganizationsStorageOrganizationsResponse.class, asyncResultHandler);
   }
 
   @Override
   @Validate
-  public void getOrganizationStorageOrganizationsById(String id, String lang, Map<String, String> okapiHeaders,
+  public void getOrganizationsStorageOrganizationsById(String id, String lang, Map<String, String> okapiHeaders,
                                            Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    PgUtil.getById(ORGANIZATION_TABLE, Organization.class, id, okapiHeaders,vertxContext, GetOrganizationStorageOrganizationsByIdResponse.class, asyncResultHandler);
+    PgUtil.getById(ORGANIZATION_TABLE, Organization.class, id, okapiHeaders,vertxContext, GetOrganizationsStorageOrganizationsByIdResponse.class, asyncResultHandler);
   }
 
   @Override
   @Validate
-  public void deleteOrganizationStorageOrganizationsById(String id, String lang, Map<String, String> okapiHeaders,
+  public void deleteOrganizationsStorageOrganizationsById(String id, String lang, Map<String, String> okapiHeaders,
                                               Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    PgUtil.deleteById(ORGANIZATION_TABLE, id, okapiHeaders, vertxContext, DeleteOrganizationStorageOrganizationsByIdResponse.class, asyncResultHandler);
+    PgUtil.deleteById(ORGANIZATION_TABLE, id, okapiHeaders, vertxContext, DeleteOrganizationsStorageOrganizationsByIdResponse.class, asyncResultHandler);
   }
 
   @Override
   @Validate
-  public void putOrganizationStorageOrganizationsById(String id, String lang, Organization entity,
+  public void putOrganizationsStorageOrganizationsById(String id, String lang, Organization entity,
                                            Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    PgUtil.put(ORGANIZATION_TABLE, entity, id, okapiHeaders, vertxContext, PutOrganizationStorageOrganizationsByIdResponse.class, asyncResultHandler);
+    PgUtil.put(ORGANIZATION_TABLE, entity, id, okapiHeaders, vertxContext, PutOrganizationsStorageOrganizationsByIdResponse.class, asyncResultHandler);
   }
 }
