6dc0dc931ef11e6f1636af4c226478506a7ee66f
Viachaslau Khandaramai <Viachaslau_Khandramai@epam.com>
2020-01-15 11:50:41 +0300

    MODORGS-53 # Migration script updating

--- src/main/resources/templates/db_scripts/migration/organizations.sql
+++ src/main/resources/templates/db_scripts/migration/organizations.sql
@@ -3,3 +3,9 @@ SET
   jsonb = jsonb || jsonb_build_object('isVendor', false)
 WHERE
   NOT jsonb ? 'isVendor';
+
+UPDATE ${myuniversity}_${mymodule}.organizations
+SET
+  jsonb = jsonb || jsonb_build_object('code', 'DEFAULT-CODE')
+WHERE
+  NOT jsonb ? 'code';
