5dcc3afb4068ce7fb28c3389d79da97b63f58d3a
Viachaslau Khandramai <Viachaslau_Khandramai@epam.com>
2020-01-27 15:36:07 +0300

    MODORGS-53 # Reverting migration script

--- src/main/resources/templates/db_scripts/migration/organizations.sql
+++ src/main/resources/templates/db_scripts/migration/organizations.sql
@@ -3,9 +3,3 @@ SET
   jsonb = jsonb || jsonb_build_object('isVendor', false)
 WHERE
   NOT jsonb ? 'isVendor';
-
-UPDATE ${myuniversity}_${mymodule}.organizations
-SET
-  jsonb = jsonb || jsonb_build_object('code', 'DEFAULT-CODE')
-WHERE
-  NOT jsonb ? 'code';
