575caeaf7075320e6b5876f7ac2a8e1c6233b76b
Siarhei_Hrabko <siarhei_hrabko@epam.com>
2020-08-21 08:49:28 +0300

    MODORGSTOR-77 typo

--- src/main/resources/templates/db_scripts/migration/make_organization_export_to_accounting_default.sql
+++ src/main/resources/templates/db_scripts/migration/make_organization_export_to_accounting_default.sql
@@ -1,4 +1,4 @@
--- set default false value for organization.enclosureNeeded
+-- set default false value for organizations.exportToAccounting
 UPDATE ${myuniversity}_${mymodule}.organizations
 	SET jsonb=jsonb || jsonb_build_object('exportToAccounting', false)
 	WHERE NOT jsonb ? 'exportToAccounting';
