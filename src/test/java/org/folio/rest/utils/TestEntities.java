686d258558c78de0b4294f57bcac23d864ea0633
alb3rtino <alb3rtino@users.noreply.github.com>
2022-01-30 13:09:11 +0100

    Add samples and reference data

--- src/test/java/org/folio/rest/utils/TestEntities.java
+++ src/test/java/org/folio/rest/utils/TestEntities.java
@@ -6,27 +6,30 @@ import org.folio.rest.jaxrs.model.Contact;
 import org.folio.rest.jaxrs.model.Email;
 import org.folio.rest.jaxrs.model.Interface;
 import org.folio.rest.jaxrs.model.Organization;
+import org.folio.rest.jaxrs.model.OrganizationType;
 import org.folio.rest.jaxrs.model.PhoneNumber;
 import org.folio.rest.jaxrs.model.Url;
 
 public enum TestEntities {
-  ADDRESS("/organizations-storage/addresses", Address.class, "address.sample", "city", "Boston ", 0),
-  CATEGORY("/organizations-storage/categories", Category.class, "category.sample", "value", "AccountingServices", 4),
-  CONTACT("/organizations-storage/contacts", Contact.class, "contact.sample",  "notes", "ABC123", 13),
-  EMAIL("/organizations-storage/emails", Email.class, "email.sample", "value", "test@folio.org", 0),
-  INTERFACE("/organizations-storage/interfaces", Interface.class, "interface.sample", "name", "Test Portal", 13),
-  ORGANIZATION("/organizations-storage/organizations", Organization.class, "organization.sample",  "code", "ABC123", 16),
-  PHONE_NUMBER("/organizations-storage/phone-numbers", PhoneNumber.class, "phoneNumber.sample", "phoneNumber", "9999999999", 0),
-  URL("/organizations-storage/urls", Url.class, "url.sample", "value", "http://test.org", 0);
+  ADDRESS("/organizations-storage/addresses", Address.class, "address.sample", "city", "Boston ", 0, false),
+  CATEGORY("/organizations-storage/categories", Category.class, "category.sample", "value", "AccountingServices", 4, true),
+  CONTACT("/organizations-storage/contacts", Contact.class, "contact.sample",  "notes", "ABC123", 13, false),
+  EMAIL("/organizations-storage/emails", Email.class, "email.sample", "value", "test@folio.org", 0, false),
+  INTERFACE("/organizations-storage/interfaces", Interface.class, "interface.sample", "name", "Test Portal", 13, false),
+  ORGANIZATION("/organizations-storage/organizations", Organization.class, "organization.sample",  "code", "ABC123", 16, false),
+  ORGANIZATION_TYPE("/organizations-storage/organization-types", OrganizationType.class, "organization_type.sample", "status", "Inactive", 4, true),
+  PHONE_NUMBER("/organizations-storage/phone-numbers", PhoneNumber.class, "phoneNumber.sample", "phoneNumber", "9999999999", 0, false),
+  URL("/organizations-storage/urls", Url.class, "url.sample", "value", "http://test.org", 0, false);
 
 
-  TestEntities(String endpoint, Class<?> clazz, String sampleFileName, String updatedFieldName, String updatedFieldValue, int initialQuantity) {
+  TestEntities(String endpoint, Class<?> clazz, String sampleFileName, String updatedFieldName, String updatedFieldValue, int initialQuantity, boolean isReferenceData) {
     this.endpoint = endpoint;
     this.clazz = clazz;
     this.sampleFileName = sampleFileName;
     this.updatedFieldName = updatedFieldName;
     this.updatedFieldValue = updatedFieldValue;
     this.initialQuantity = initialQuantity;
+    this.isReferenceData = isReferenceData;
   }
 
   private int initialQuantity;
@@ -34,6 +37,7 @@ public enum TestEntities {
   private String sampleFileName;
   private String updatedFieldName;
   private Object updatedFieldValue;
+  private boolean isReferenceData;
   private Class<?> clazz;
 
   public String getEndpoint() {
@@ -62,5 +66,9 @@ public enum TestEntities {
 
   public Class<?> getClazz() {
     return clazz;
+  }
+
+  public boolean isReferenceData() {
+    return isReferenceData;
   }
 }
