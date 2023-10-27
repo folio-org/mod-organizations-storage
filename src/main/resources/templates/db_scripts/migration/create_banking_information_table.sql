02823127321b2fefc393abeec96cf2f0653134d8
Dzmitry_Butramyou <dzmitry_butramyou@epam.com>
2023-10-27 17:01:01 +0400

    [MODORG-54] - Refactoring

--- src/main/resources/templates/db_scripts/migration/create_banking_information_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS banking_information (
-  id uuid NOT NULL PRIMARY KEY,
-  jsonb JSONB NOT NULL,
-  organization_id  uuid,
-  constraint ORG_BANK_INFO_FK foreign key (organization_id)
-  references organizations(id)
-);
--- src/test/java/org/folio/rest/impl/BankingInformationTest.java
+++ src/test/java/org/folio/rest/impl/BankingInformationTest.java
@@ -3,6 +3,7 @@ package org.folio.rest.impl;
 import io.vertx.core.json.JsonObject;
 import org.folio.rest.jaxrs.model.BankingInformation;
 import org.folio.rest.jaxrs.model.BankingInformationCollection;
+import org.folio.rest.utils.TestEntities;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
@@ -14,10 +15,12 @@ import static org.folio.rest.utils.TenantApiTestUtil.prepareTenant;
 import static org.folio.rest.utils.TenantApiTestUtil.purge;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class BankingInformationTest extends TestBase {
+class BankingInformationTest extends TestBase {
 
   private static final String BANKING_INFORMATION_ENDPOINT = "/organizations-storage/banking-information";
   private static final String BANKING_INFORMATION_ENDPOINT_WITH_ID = "/organizations-storage/banking-information/{id}";
+  private static final String ORGANIZATIONS_ENDPOINT = TestEntities.ORGANIZATION.getEndpoint();
+  private static final String ORGANIZATION_SAMPLE_FILE = "organization.sample";
 
   @AfterAll
   static void afterAll() {
@@ -27,9 +30,16 @@ public class BankingInformationTest extends TestBase {
 
   @Test
   void testBankingInformationEndpoints() throws MalformedURLException {
+    String organizationId =
+      postData(ORGANIZATIONS_ENDPOINT, getFile(ORGANIZATION_SAMPLE_FILE))
+        .then()
+        .statusCode(201)
+        .extract()
+        .path("id");
+
     BankingInformation bankingInformation = new BankingInformation()
       .withId(UUID.randomUUID().toString())
-      .withOrganizationId(UUID.randomUUID().toString())
+      .withOrganizationId(organizationId)
       .withBankName("Bank name")
       .withTransitNumber("123456");
 
@@ -45,6 +55,12 @@ public class BankingInformationTest extends TestBase {
       .body().as(BankingInformation.class);
     assertEquals(bankingInformation.getBankName(), createdBankingInformation.getBankName());
 
+    createdBankingInformation.setBankName("New Bank name");
+
+    putData(BANKING_INFORMATION_ENDPOINT_WITH_ID, createdBankingInformation.getId(), JsonObject.mapFrom(createdBankingInformation).encodePrettily())
+      .then()
+      .statusCode(204);
+
     List<BankingInformation> bankingInformationList = getData(BANKING_INFORMATION_ENDPOINT)
       .then()
       .log().ifValidationFails()
@@ -52,7 +68,7 @@ public class BankingInformationTest extends TestBase {
       .extract()
       .body().as(BankingInformationCollection.class).getBankingInformation();
     assertEquals(1, bankingInformationList.size());
-    assertEquals(bankingInformation.getBankName(), bankingInformationList.get(0).getBankName());
+    assertEquals(createdBankingInformation.getBankName(), bankingInformationList.get(0).getBankName());
 
     deleteDataSuccess(BANKING_INFORMATION_ENDPOINT_WITH_ID, bankingInformation.getId());
   }
