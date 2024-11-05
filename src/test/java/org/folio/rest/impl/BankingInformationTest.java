050303974110fc171ff311c40449d7cde176aa69
Serhii Nosko <v-snosko@ebsco.com>
2024-11-05 13:06:30 +0200

    MODORGSTOR-172. Error message when an organization cannot be deleted because of banking information (#124)
    * MODORGSTOR-172. Error message when an organization cannot be deleted because of banking information
    
    (cherry picked from commit 83ac12a07eceeec0ca7a4e1c4146b6765eb12956)

--- src/test/java/org/folio/rest/impl/BankingInformationTest.java
+++ src/test/java/org/folio/rest/impl/BankingInformationTest.java
@@ -20,7 +20,9 @@ class BankingInformationTest extends TestBase {
   private static final String BANKING_INFORMATION_ENDPOINT = "/organizations-storage/banking-information";
   private static final String BANKING_INFORMATION_ENDPOINT_WITH_ID = "/organizations-storage/banking-information/{id}";
   private static final String ORGANIZATIONS_ENDPOINT = TestEntities.ORGANIZATION.getEndpoint();
+  private static final String ORGANIZATIONS_ENDPOINT_WITH_ID = ORGANIZATIONS_ENDPOINT + "/{id}";
   private static final String ORGANIZATION_SAMPLE_FILE = "organization.sample";
+  private static final String ORGANIZATION2_SAMPLE_FILE = "organization2.sample";
 
   @AfterAll
   static void afterAll() {
@@ -37,11 +39,7 @@ class BankingInformationTest extends TestBase {
         .extract()
         .path("id");
 
-    BankingInformation bankingInformation = new BankingInformation()
-      .withId(UUID.randomUUID().toString())
-      .withOrganizationId(organizationId)
-      .withBankName("Bank name")
-      .withTransitNumber("123456");
+    BankingInformation bankingInformation = createBankingInformation(organizationId, "Bank name", "123456");
 
     postData(BANKING_INFORMATION_ENDPOINT, JsonObject.mapFrom(bankingInformation).encodePrettily())
       .then()
@@ -71,5 +69,85 @@ class BankingInformationTest extends TestBase {
     assertEquals(createdBankingInformation.getBankName(), bankingInformationList.get(0).getBankName());
 
     deleteDataSuccess(BANKING_INFORMATION_ENDPOINT_WITH_ID, bankingInformation.getId());
+    deleteDataSuccess(ORGANIZATIONS_ENDPOINT_WITH_ID, organizationId);
+  }
+
+  @Test
+  void deleteOrganizationWithMultipleBankingInformation() throws MalformedURLException {
+    // Create organization
+    String organizationId =
+      postData(ORGANIZATIONS_ENDPOINT, getFile(ORGANIZATION_SAMPLE_FILE))
+        .then()
+        .statusCode(201)
+        .extract()
+        .path("id");
+
+    // Create 2 banking information for organization
+    BankingInformation bankingInformation1 = createBankingInformation(organizationId, "Bank name1", "123456");
+    BankingInformation bankingInformation2 = createBankingInformation(organizationId, "Bank name2", "654321");
+    postData(BANKING_INFORMATION_ENDPOINT, JsonObject.mapFrom(bankingInformation1).encodePrettily())
+      .then()
+      .statusCode(201);
+    postData(BANKING_INFORMATION_ENDPOINT, JsonObject.mapFrom(bankingInformation2).encodePrettily())
+      .then()
+      .statusCode(201);
+
+    // Delete organization
+    deleteDataSuccess(ORGANIZATIONS_ENDPOINT_WITH_ID, organizationId);
+
+    // Verify both banking information do not exist anymore
+    testVerifyEntityDeletion(BANKING_INFORMATION_ENDPOINT_WITH_ID, bankingInformation1.getId());
+    testVerifyEntityDeletion(BANKING_INFORMATION_ENDPOINT_WITH_ID, bankingInformation2.getId());
+  }
+
+  @Test
+  void testDeleteFirstOrganizationWithBankingInformation() throws MalformedURLException {
+    // Create first organization
+    String organizationId1 =
+      postData(ORGANIZATIONS_ENDPOINT, getFile(ORGANIZATION_SAMPLE_FILE))
+        .then()
+        .statusCode(201)
+        .extract()
+        .path("id");
+
+    // Create second organization
+    String organizationId2 =
+      postData(ORGANIZATIONS_ENDPOINT, getFile(ORGANIZATION2_SAMPLE_FILE))
+        .then()
+        .statusCode(201)
+        .extract()
+        .path("id");
+
+    // Create banking information for first organization
+    BankingInformation bankingInformation1 = createBankingInformation(organizationId1, "Bank name1", "123456");
+    postData(BANKING_INFORMATION_ENDPOINT, JsonObject.mapFrom(bankingInformation1).encodePrettily())
+      .then()
+      .statusCode(201);
+
+    // Create banking information for second organization
+    BankingInformation bankingInformation2 = createBankingInformation(organizationId2, "Bank name2", "654321");
+    postData(BANKING_INFORMATION_ENDPOINT, JsonObject.mapFrom(bankingInformation2).encodePrettily())
+      .then()
+      .statusCode(201);
+
+    // Delete first organization
+    deleteDataSuccess(ORGANIZATIONS_ENDPOINT_WITH_ID, organizationId1);
+    testVerifyEntityDeletion(BANKING_INFORMATION_ENDPOINT_WITH_ID, bankingInformation1.getId());
+
+    // Verify banking information of second organization still exists
+    BankingInformation fetchedBankingInformation2 = getData(String.format("%s/%s", BANKING_INFORMATION_ENDPOINT, bankingInformation2.getId()))
+      .then()
+      .statusCode(200)
+      .extract()
+      .body().as(BankingInformation.class);
+    assertEquals(bankingInformation2.getBankName(), fetchedBankingInformation2.getBankName());
+  }
+
+  private BankingInformation createBankingInformation(String organizationId, String bankName, String transitNumber) {
+    return new BankingInformation()
+      .withId(UUID.randomUUID().toString())
+      .withOrganizationId(organizationId)
+      .withBankName(bankName)
+      .withTransitNumber(transitNumber);
   }
 }
