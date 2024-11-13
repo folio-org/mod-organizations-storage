b654fb4bea4afe2a22a2fbd87605be59dc686334
saba_zedginidze <saba_zedginidze@epam.com>
2024-11-13 14:43:49 +0400

    [MODORGSTOR-164] Add spring with DI support

--- src/main/java/org/folio/service/OrganizationService.java
+++ src/main/java/org/folio/service/OrganizationService.java
@@ -1,5 +1,6 @@
 package org.folio.service;
 
+import static org.folio.dao.DbUtils.getPgClient;
 import static org.folio.rest.utils.ResponseUtils.buildBadRequestResponse;
 import static org.folio.rest.utils.ResponseUtils.buildErrorResponse;
 import static org.folio.rest.utils.ResponseUtils.buildNoContentResponse;
@@ -17,7 +18,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.folio.dao.organization.OrganizationDAO;
 import org.folio.rest.jaxrs.model.Organization;
 import org.folio.rest.jaxrs.model.OrganizationAuditEvent;
-import org.folio.rest.persist.PostgresClient;
 import org.folio.service.audit.AuditOutboxService;
 
 import javax.ws.rs.core.Response;
@@ -29,12 +29,12 @@ public class OrganizationService {
 
   private final OrganizationDAO organizationDAO;
   private final AuditOutboxService auditOutboxService;
-  private final PostgresClient pgClient;
 
   public void createOrganization(Organization organization, Map<String, String> headers,
                                  Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     log.info("createOrganization:: Creating a new organization by id: {}", organization.getId());
-    pgClient.withTrans(conn -> organizationDAO.createOrganization(organization, conn)
+    getPgClient(vertxContext, headers)
+      .withTrans(conn -> organizationDAO.createOrganization(organization, conn)
       .compose(id -> auditOutboxService.saveOrganizationOutboxLog(conn, organization, OrganizationAuditEvent.Action.CREATE, headers)))
       .onSuccess(s -> {
         log.info("createOrganization:: Successfully created a new organization by id: {}", organization.getId());
@@ -54,7 +54,8 @@ public class OrganizationService {
     if (StringUtils.isBlank(id)) {
       asyncResultHandler.handle(buildBadRequestResponse("Organization id is required"));
     }
-    pgClient.withTrans(conn -> organizationDAO.updateOrganization(id, organization, conn)
+    getPgClient(vertxContext, headers)
+      .withTrans(conn -> organizationDAO.updateOrganization(id, organization, conn)
       .compose(organizationId -> auditOutboxService.saveOrganizationOutboxLog(conn, organization, OrganizationAuditEvent.Action.EDIT, headers)))
       .onSuccess(s -> {
         log.info("updateOrganization:: Successfully updated organization with id: {}", id);
@@ -67,8 +68,10 @@ public class OrganizationService {
       });
   }
 
-  public void deleteOrganization(String id, Handler<AsyncResult<Response>> asyncResultHandler) {
-    pgClient.withTrans(conn -> organizationDAO.deleteOrganization(id, conn))
+  public void deleteOrganization(String id, Map<String, String> headers,
+                                 Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    getPgClient(vertxContext, headers)
+      .withTrans(conn -> organizationDAO.deleteOrganization(id, conn))
       .onSuccess(rowSet -> {
         log.info("deleteOrganization:: Organization with id: '{}' and associated data were successfully deleted", id);
         asyncResultHandler.handle(buildNoContentResponse());
