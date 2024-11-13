622fcbde2d6e7ae4cea8884fef43152567ba39a8
saba_zedginidze <saba_zedginidze@epam.com>
2024-11-13 16:20:55 +0400

    [MODORGSTOR-164] Init spring on startup

---
+++ src/main/java/org/folio/rest/impl/InitAPIs.java
@@ -0,0 +1,43 @@
+package org.folio.rest.impl;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.folio.config.ApplicationConfig;
+import org.folio.dbschema.ObjectMapperTool;
+import org.folio.rest.resource.interfaces.InitAPI;
+import org.folio.spring.SpringContextUtil;
+
+import com.fasterxml.jackson.databind.DeserializationConfig;
+import com.fasterxml.jackson.databind.SerializationConfig;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.jackson.DatabindCodec;
+
+/**
+ * The class initializes vertx context adding spring context
+ */
+public class InitAPIs implements InitAPI {
+  private static final Logger log = LogManager.getLogger(InitAPIs.class);
+
+  @Override
+  public void init(Vertx vertx, Context context, Handler<AsyncResult<Boolean>> resultHandler) {
+    vertx.executeBlocking(
+      promise -> {
+        SpringContextUtil.init(vertx, context, ApplicationConfig.class);
+        promise.complete();
+      },
+      result -> {
+        if (result.succeeded()) {
+          log.info("APIs initialized successfully");
+          resultHandler.handle(Future.succeededFuture(true));
+        } else {
+          log.error("Failure to init API", result.cause());
+          resultHandler.handle(Future.failedFuture(result.cause()));
+        }
+      });
+  }
+}
