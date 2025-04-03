b05dae0cd4c4c3cea99c18b53c5ed90cd1270983
Damien <damien-git@users.noreply.github.com>
2025-04-03 14:07:55 -0400

    [MODORGSTOR-178] Removed unused tables and APIs

--- src/test/java/org/folio/rest/utils/TestEntities.java
+++ src/test/java/org/folio/rest/utils/TestEntities.java
@@ -1,26 +1,18 @@
 package org.folio.rest.utils;
 
-import org.folio.rest.jaxrs.model.Address;
 import org.folio.rest.jaxrs.model.Category;
 import org.folio.rest.jaxrs.model.Contact;
-import org.folio.rest.jaxrs.model.Email;
 import org.folio.rest.jaxrs.model.Interface;
 import org.folio.rest.jaxrs.model.Organization;
 import org.folio.rest.jaxrs.model.OrganizationType;
-import org.folio.rest.jaxrs.model.PhoneNumber;
-import org.folio.rest.jaxrs.model.Url;
 
 public enum TestEntities {
-  ADDRESS("/organizations-storage/addresses", Address.class, "address.sample", "city", "Boston ", 0, false),
   CATEGORY("/organizations-storage/categories", Category.class, "category.sample", "value", "AccountingServices", 4, true),
   CONTACT("/organizations-storage/contacts", Contact.class, "contact.sample", "notes", "ABC123", 13, false),
   PRIVILEGED_CONTACT("/organizations-storage/privileged-contacts", Contact.class, "contact.sample", "notes", "ABC123", 0, false),
-  EMAIL("/organizations-storage/emails", Email.class, "email.sample", "value", "test@folio.org", 0, false),
   INTERFACE("/organizations-storage/interfaces", Interface.class, "interface.sample", "name", "Test Portal", 13, false),
   ORGANIZATION("/organizations-storage/organizations", Organization.class, "organization.sample", "code", "ABC123", 16, false),
-  ORGANIZATION_TYPE("/organizations-storage/organization-types", OrganizationType.class, "organization_type.sample", "status", "Inactive", 4, true),
-  PHONE_NUMBER("/organizations-storage/phone-numbers", PhoneNumber.class, "phoneNumber.sample", "phoneNumber", "9999999999", 0, false),
-  URL("/organizations-storage/urls", Url.class, "url.sample", "value", "http://test.org", 0, false);
+  ORGANIZATION_TYPE("/organizations-storage/organization-types", OrganizationType.class, "organization_type.sample", "status", "Inactive", 4, true);
 
 
   TestEntities(String endpoint, Class<?> clazz, String sampleFileName, String updatedFieldName, String updatedFieldValue, int initialQuantity, boolean isReferenceData) {
