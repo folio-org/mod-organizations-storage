05f895a336b951ebe9e4f184a21ee5d0281375c6
Boburbek Kadirkhodjaev <b.kadirkhodjaev@gmail.com>
2025-07-18 19:23:03 +0500

    [MODORG-84]. ECS - Create reference organizations for each tenant (#143)

--- src/test/java/org/folio/rest/utils/TestEntities.java
+++ src/test/java/org/folio/rest/utils/TestEntities.java
@@ -9,11 +9,18 @@ import org.folio.rest.jaxrs.model.OrganizationType;
 public enum TestEntities {
   CATEGORY("/organizations-storage/categories", Category.class, "category.sample", "value", "AccountingServices", 4, true),
   CONTACT("/organizations-storage/contacts", Contact.class, "contact.sample", "notes", "ABC123", 13, false),
-  PRIVILEGED_CONTACT("/organizations-storage/privileged-contacts", Contact.class, "contact.sample", "notes", "ABC123", 0, false),
+  PRIVILEGED_CONTACT("/organizations-storage/privileged-contacts", Contact.class, "contact.sample", "notes", "ABC123", 2, false),
   INTERFACE("/organizations-storage/interfaces", Interface.class, "interface.sample", "name", "Test Portal", 13, false),
-  ORGANIZATION("/organizations-storage/organizations", Organization.class, "organization.sample", "code", "ABC123", 16, false),
+  ORGANIZATION("/organizations-storage/organizations", Organization.class, "organization.sample", "code", "ABC123", 23, false),
   ORGANIZATION_TYPE("/organizations-storage/organization-types", OrganizationType.class, "organization_type.sample", "status", "Inactive", 4, true);
 
+  private final int initialQuantity;
+  private final String endpoint;
+  private final String sampleFileName;
+  private final String updatedFieldName;
+  private final Object updatedFieldValue;
+  private final boolean isReferenceData;
+  private final Class<?> clazz;
 
   TestEntities(String endpoint, Class<?> clazz, String sampleFileName, String updatedFieldName, String updatedFieldValue, int initialQuantity, boolean isReferenceData) {
     this.endpoint = endpoint;
@@ -24,14 +31,6 @@ public enum TestEntities {
     this.initialQuantity = initialQuantity;
     this.isReferenceData = isReferenceData;
   }
-
-  private int initialQuantity;
-  private String endpoint;
-  private String sampleFileName;
-  private String updatedFieldName;
-  private Object updatedFieldValue;
-  private boolean isReferenceData;
-  private Class<?> clazz;
 
   public String getEndpoint() {
     return endpoint;
