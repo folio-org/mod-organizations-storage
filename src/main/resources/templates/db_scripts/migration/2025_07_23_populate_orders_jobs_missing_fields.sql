f145c22abdf4a055e29c516ef4256fcf92a90277
saba_zedginidze <saba_zedginidze@epam.com>
2025-07-23 14:26:07 +0400

    Create migration to populate integration type 'Ordering' in mod-configurations

---
+++ src/main/resources/templates/db_scripts/migration/2025_07_23_populate_orders_jobs_missing_fields.sql
@@ -0,0 +1,24 @@
+UPDATE ${myuniversity}_mod_configuration.config_data
+SET jsonb = jsonb_set(
+      jsonb,
+      '{value}',
+      to_jsonb(
+        (jsonb_set(
+          (jsonb->>'value')::jsonb,
+          '{exportTypeSpecificParameters,vendorEdiOrdersExportConfig}',
+          ((jsonb->>'value')::jsonb->'exportTypeSpecificParameters'->'vendorEdiOrdersExportConfig') ||
+          jsonb_build_object(
+            'integrationType', 'Ordering',
+            'transmissionMethod', 'FTP',
+            'fileFormat', 'EDI'
+          ),
+          true
+        ))::text
+      )
+    )
+WHERE jsonb->>'module' = 'mod-data-export-spring' AND jsonb ? 'value'
+  AND (jsonb->>'value')::jsonb->>'type' = 'EDIFACT_ORDERS_EXPORT'
+  AND ((jsonb->>'value')::jsonb->'exportTypeSpecificParameters' ? 'vendorEdiOrdersExportConfig')
+  AND NOT (((jsonb->>'value')::jsonb->'exportTypeSpecificParameters'->'vendorEdiOrdersExportConfig') ? 'integrationType')
+  AND NOT (((jsonb->>'value')::jsonb->'exportTypeSpecificParameters'->'vendorEdiOrdersExportConfig') ? 'transmissionMethod')
+  AND NOT (((jsonb->>'value')::jsonb->'exportTypeSpecificParameters'->'vendorEdiOrdersExportConfig') ? 'fileFormat');
